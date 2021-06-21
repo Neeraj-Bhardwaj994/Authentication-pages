6972254646c3945df8cde6ee1cf6364266baec65
Neeraj <73283359+Neeraj-Bhardwaj994@users.noreply.github.com>
2021-06-21 12:33:46 +0530

    Files upload

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://neeraj1:neeraj1@cluster0.uhvuz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport')
+const { ensureAuthenticated } = require('../config/auth')
+
+//Welcome page
+router.get('/',(req,res) => {
+    res.render("welcome")
+})
+
+// Dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) =>
+  res.render('dashboard', {
+    user: req.user
+  })
+)
+
+module.exports = router
